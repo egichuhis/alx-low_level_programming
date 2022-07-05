f405a05c33e861929c501ae0e49744b30371a77b
edwin gichuhi <edwingichuhis@gmail.com>
2022-07-05 16:59:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,15 +6,21 @@
 
 /**
  *
- * main - Main function of my project
+ * *main - Main function of my project
  *
- * Return: return 0 everytime
+ * *
+ *
+ * *
+ *
+ * *Return: return 0 everytime
  *
  * */
 
 int main(void)
 
 {
+
+
 
 		char displayed[52];
 
