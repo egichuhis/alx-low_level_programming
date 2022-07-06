916ca6c7c2421b5afcba5a39d766d75780cfee1c
edwin gichuhi <edwingichuhis@gmail.com>
2022-07-05 17:10:17 -0700

    del file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -15,9 +15,8 @@
 
 int main(void)
 {
-	char displayed[52];
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
-
-	return (0);
+char displayed[52];
+strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+puts(displayed);
+return (0);
 }
